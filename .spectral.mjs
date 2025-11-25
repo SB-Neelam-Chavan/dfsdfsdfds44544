f57199b7dbd43690b29a2437046504f179b04925
sdsd34343sdfd33434343444445454dfdfdfsdf <neelam.chavan@smartbear.com>
2025-11-25 08:56:09 +0530

    Added 947 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://integration.stoplight-dev.com/api/v1/projects/cHJqOjcxOTg4Nw/spectral.js?branch_slug=main&token=87bc002c-61ac-43db-ab3a-aadaa38b933b";
+export default { extends: ruleset };
